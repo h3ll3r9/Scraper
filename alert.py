cd5c85d0043b1a6c314a418a1d0aa8ad133736e9
h3ll3r9 <103018033+h3ll3r9@users.noreply.github.com>
2023-04-05 21:28:39 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,14 +1,14 @@
 import smtplib
 from email.message import EmailMessage
 
-def alert_mail():
+def alert_mail(title, price, email_addr,link):
         email_id = 'spidyscraper@gmail.com'
-        password = '********' #Password
+        password = '#####' #Password
         msg = EmailMessage()
         msg['Subject'] = 'Price dropped'
         msg['From'] = email_id
-        msg['To'] = 'aryanbharti9.9@gmail.com'
-        msg.set_content("Hi, The price of product is drooped")
+        msg['To'] = email_addr
+        msg.set_contentprint(f"Hi, The price of {title} is drooped to {price}.\n Link: {link}")
         with smtplib.SMTP_SSL('smtp.gmail.com',587) as smtp:
             smtp.login(email_id,password)
             smtp.send_message(msg)
